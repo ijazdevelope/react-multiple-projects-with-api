0373c6a5d395c22894337625099e39e5e09b5339
Muhammad Ijaz <ijazlovely00786@gmail.com>
2023-04-05 11:39:38 +0500

    write a new delete constans action-type in actions

--- src/redux/constants/Constants.js
+++ src/redux/constants/Constants.js
@@ -1,6 +1,7 @@
 export const actionTypes = {
     ADD_TODO: 'ADD_TODO',
-    DELETE_TODO: 'DELETE_TODO'
+    DELETE_TODO: 'DELETE_TODO',
+    EDIT_TODO: 'EDIT_TODO'
 }
 
 export const INCREMENT = 'INCREMENT';
