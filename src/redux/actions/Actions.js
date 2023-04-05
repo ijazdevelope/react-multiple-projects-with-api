7940cdb826351953c4e5fb5cee74f5c24eea94f0
Muhammad Ijaz <ijazlovely00786@gmail.com>
2023-04-05 11:38:27 +0500

    write a update todo function to update the todo

--- src/redux/actions/Actions.js
+++ src/redux/actions/Actions.js
@@ -15,9 +15,16 @@ export const addTodo = (payload) => {
 }
 
 export const deleteTodo = (payload) => {
-    console.log(payload, 'delete todo in actions...')
     return {
         type: actionTypes.DELETE_TODO,
+        payload
+    }
+}
+
+export const updateTodo = (payload) => {
+    console.log(payload, 'payload in actions...')
+    return {
+        type: actionTypes.EDIT_TODO,
         payload
     }
 }
