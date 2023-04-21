a8e685b663f527a408a8e3b7dc159e1b2a0147f6
Muhammad Ijaz <ijazlovely00786@gmail.com>
2023-04-21 22:45:50 +0500

    implemented dark and light mood

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import ijazImg from '../static/images/ijaz.jpeg';
 
-const Navbar = () => {
+const Navbar = ({ mood, toggleMood }) => {
+
     return (
-        <nav className="navbar navbar-expand-lg bg-light">
+        <nav className={`navbar navbar-expand-lg bg-${mood === 'dark' ? 'dark' : 'light'}`}>
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <Link className="navbar-brand" to="/">
+                    <img src={ijazImg} style={{ height: '40px' }} className='rounded' alt="ijaz-group" />
+                </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link to='/list-group' className="nav-link active" aria-current="page">List Group</Link>
+                            <Link to='/list-group' className={`nav-link active ${mood === 'dark' && 'text-white'} `} aria-current="page">List Group</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/products">E-commerce</Link>
+                            <Link className={`nav-link ${mood === 'dark' && 'text-white'} `} to="/products">E-commerce</Link>
                         </li>
                         <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <a className={`nav-link dropdown-toggle ${mood === 'dark' && 'text-white'}`} href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Dropdown
                             </a>
                             <ul className="dropdown-menu">
@@ -30,13 +34,22 @@ const Navbar = () => {
                             </ul>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link disabled">Disabled</a>
+                            <a className="nav-link disabled" style={mood === 'dark' ? { color: '#878181' } : { color: 'rgba(0, 0, 0, 0.3)' }} >Disabled</a>
                         </li>
                     </ul>
                     <form className="d-flex" role="search">
                         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                         <button className="btn btn-outline-success" type="submit">Search</button>
                     </form>
+                    <div className="form-check form-switch ms-2">
+                        <input className="form-check-input" onClick={toggleMood} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                        <label
+                            className={`form-check-label ${mood === 'dark' ? 'text-white' : 'text-black'}`}
+                            htmlFor="flexSwitchCheckDefault"
+                        >
+                            {mood === 'light' ? 'Dark Mood' : 'Light Mood'}
+                        </label>
+                    </div>
                 </div>
             </div>
         </nav>
